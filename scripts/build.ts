ad514216c4e8d3140596bca5acc549170b9d4ed1
Nicolas Dubois <DuboisNicolasToulouse@users.noreply.github.com>
2026-02-13 16:45:42 +0100

    Make build.sh incremental

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,3 +1,4 @@
+#!npx tsx
 import { Command, Option } from "commander";
 import fs from "fs";
 import path from "path";
@@ -29,6 +30,39 @@ function pathLookup(seachedFile: string): string | undefined {
 }
 
 
+//
+// Return true if frist file is newer (strictly)
+//
+function isFirstNewer(firstFile: string, secondFile: string): boolean {
+  if (!fs.existsSync(secondFile)) return true;
+  const { mtime: firstMtime } = fs.statSync(firstFile);
+  const { mtime: secondMtime } = fs.statSync(secondFile);
+  return firstMtime > secondMtime;
+}
+
+//
+// Copy sourceFile to secondFile if sourceFile is newer.
+// Work recursivelly with directories.
+//
+function cpIfNewer(sourceFile: string, secondFile: string, basePathForLog = "") {
+  if (fs.statSync(sourceFile).isDirectory()) {
+    fs.mkdirSync(secondFile, { recursive: true });
+    fs.readdirSync(sourceFile).forEach((file) => {
+      cpIfNewer(
+        path.join(sourceFile, file),
+        path.join(secondFile, file),
+        path.join(basePathForLog, path.basename(sourceFile), ""),
+      );
+    });
+  } else {
+    if (isFirstNewer(sourceFile, secondFile)) {
+      console.log("Install " + path.join(basePathForLog, path.basename(secondFile)) + "...");
+      fs.cpSync(sourceFile, secondFile);
+    }
+  }
+}
+
+
 const program_root = path.normalize(import.meta.dirname + "/..");
 
 //
@@ -41,37 +75,45 @@ argsParser.addOption(new Option("-t, --target <path>", "path to target folder (a
   .default("build"));
 argsParser.addOption(new Option("--keep-db", "Copy the database from dev environment to the delivery.")
   .default(false));
+argsParser.addOption(new Option("-c --clean", "clean the delivery before build (rebuild all).")
+  .default(false));
 
 argsParser.parse();
-
+const options = argsParser.opts();
 
 //
 // Create/clean output directory
 //
-let target: string;
-if (path.isAbsolute(argsParser.opts().target)) {
-  target = argsParser.opts().target;
-} else {
-  target = path.join(program_root, argsParser.opts().target);
-}
-console.log("Clean/Create target dir '" + target + "'.");
-if (fs.existsSync(target)) {
+const target = (path.isAbsolute(options.target))
+  ? options.target
+  : path.join(program_root, options.target);
+console.log("Prepare target dir '" + target + "'.");
+
+if (options.clean) {
   fs.rmSync(target, {
+    force: true,
     recursive: true,
   });
 }
-fs.mkdirSync(target);
+
+fs.mkdirSync(target, { recursive: true });
 
 
 //
-// Build vite
+// Build vite (regardless clean option)
 //
 console.log("Building vite...");
 const vite = pathLookup("vite");
 if (!vite) {
   die("vite cannot be found !");
 }
+
 const vite_target = target + "/dist";
+fs.rmSync(vite_target, {
+  force: true,
+  recursive: true,
+});
+
 execSync(`${vite} build --outDir ${vite_target} --emptyOutDir`, {
   cwd: program_root,
   stdio: [ 0, 1, 2 ],
@@ -82,36 +124,39 @@ execSync(`${vite} build --outDir ${vite_target} --emptyOutDir`, {
 // Copy needed files
 //
 console.log("Copy needed files...");
-fs.cpSync(program_root + "/package.json", target + "/package.json");
-fs.cpSync(program_root + "/LICENSE", target + "/LICENSE");
-fs.cpSync(program_root + "/tsconfig.json", target + "/tsconfig.json");
-fs.cpSync(program_root + "/vite.config.ts", target + "/vite.config.ts");
-fs.cpSync(program_root + "/schemas", target + "/schemas", { recursive: true });
-fs.cpSync(program_root + "/scripts", target + "/scripts", { recursive: true });
-fs.mkdirSync(target + "/src");
-fs.cpSync(program_root + "/src/server", target + "/src/server", { recursive: true });
+cpIfNewer(path.join(program_root, "/LICENSE"), path.join(target, "/LICENSE"));
+cpIfNewer(path.join(program_root, "/tsconfig.json"), path.join(target, "/tsconfig.json"));
+cpIfNewer(path.join(program_root, "/vite.config.ts"), path.join(target, "/vite.config.ts"));
+cpIfNewer(path.join(program_root, "/schemas"), path.join(target, "/schemas"));
+cpIfNewer(path.join(program_root, "/scripts"), path.join(target, "/scripts"));
+cpIfNewer(path.join(program_root, "/src/server"), path.join(target, "/src/server"));
 
 //
 // Copy database if required
 //
-if (argsParser.opts().keepDb) {
+if (options.keepDb) {
   console.log("Copy database...");
   // the secret is keept to keep cookies valid
-  fs.cpSync(program_root + "/jwt_secret.txt", target + "/jwt_secret.txt");
-  fs.cpSync(program_root + "/database", target + "/database", { recursive: true });
+  cpIfNewer(path.join(program_root, "/jwt_secret.txt"), path.join(target, "/jwt_secret.txt"));
+  cpIfNewer(path.join(program_root, "/database"), path.join(target, "/database"));
 }
 
 //
 // Install NPM packages
 //
-console.log("Install NPM packages...");
-const npm = pathLookup("npm");
-if (!npm) {
-  die("npm cannot be found !");
+const sourcePackageJson = path.join(program_root, "/package.json");
+const destPackageJson = path.join(target, "/package.json");
+if (isFirstNewer(sourcePackageJson, destPackageJson)) {
+  console.log("Install NPM packages...");
+  const npm = pathLookup("npm");
+  if (!npm) {
+    die("npm cannot be found !");
+  }
+  fs.cpSync(sourcePackageJson, destPackageJson);
+  execSync("npm install --omit=dev", {
+    cwd: target,
+    stdio: [ 0, 1, 2 ],
+  });
 }
-execSync("npm install --omit=dev", {
-  cwd: target,
-  stdio: [ 0, 1, 2 ],
-});
 
 console.log("Done.");
