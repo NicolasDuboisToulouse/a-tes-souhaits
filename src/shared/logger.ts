498a32f2d7b6fb2cfcb85721bd3d221fe9fb2ac3
Nicolas Dubois <DuboisNicolasToulouse@users.noreply.github.com>
2026-02-19 07:45:14 +0100

    Upgrade logger

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -1,62 +1,99 @@
 import pino from "pino";
 import pretty from "pino-pretty";
 
-
 const stream = pretty({
   colorize: true,
   translateTime: "mm-dd HH:MM:ss.l",
 });
-
 
 const logger = pino(
   { level: process.env.LOG_LEVEL ? process.env.LOG_LEVEL : "info" },
   stream,
 );
 
-type Args = string | number | object | null | undefined;
+// Argument supported by logger functions
+type ArgType = string | number | object | null | undefined;
 
-function stringify(value: Args): string {
+// Options that can be applied to an argument
+interface Options { quoted?: boolean; noSpace?: boolean }
+class OptionsArg {
+  readonly arg: ArgType;
+  readonly options: Options;
+  constructor(arg: ArgType, options: Options) {
+    this.arg = arg;
+    this.options = options;
+  }
+}
+
+export function quote(arg: ArgType | OptionsArg) {
+  if (arg instanceof OptionsArg) {
+    return new OptionsArg(arg.arg, { ...arg.options, quoted: true });
+  } else {
+    return new OptionsArg(arg, { quoted: true });
+  }
+}
+
+export function noSpace(arg: ArgType | OptionsArg) {
+  if (arg instanceof OptionsArg) {
+    return new OptionsArg(arg.arg, { ...arg.options, noSpace: true });
+  } else {
+    return new OptionsArg(arg, { noSpace: true });
+  }
+}
+
+
+// Convert arguments to string
+function stringify(value: ArgType): string {
   if (typeof value == "undefined") return "undefined";
   if (typeof value == "string") return value;
   if (typeof value == "number") return value.toString();
   return JSON.stringify(value);
 }
 
-function stringifyArgs(...args: Args[]): string {
-  let first = true;
+function stringifyArgs(...args: ArgType[]): string {
+  let prepend = "";
+  let append = "";
   let result = "";
-  for (const arg of args) {
-    if (first) {
-      result = stringify(arg);
-      first = false;
-      continue;
+  for (let arg of args) {
+    if (arg instanceof OptionsArg) {
+      if (arg.options.noSpace) {
+        prepend = "";
+      }
+      if (arg.options.quoted) {
+        prepend += "'";
+        append = "'" + append;
+      }
+      arg = arg.arg;
     }
-    result = result + " " + stringify(arg);
+    result += prepend + stringify(arg) + append;
+    prepend = " ";
+    append = "";
   }
   return result;
 }
 
-export function trace(...args: Args[]) {
+// Logger methods
+export function trace(...args: ArgType[]) {
   logger.trace(stringifyArgs(...args));
 }
 
-export function debug(...args: Args[]) {
+export function debug(...args: ArgType[]) {
   logger.debug(stringifyArgs(...args));
 }
 
-export function info(...args: Args[]) {
+export function info(...args: ArgType[]) {
   logger.info(stringifyArgs(...args));
 }
 
-export function warn(...args: Args[]) {
+export function warn(...args: ArgType[]) {
   logger.warn(stringifyArgs(...args));
 }
 
-export function warning(...args: Args[]) {
+export function warning(...args: ArgType[]) {
   logger.warn(stringifyArgs(...args));
 }
 
-export function error(...args: Args[]) {
+export function error(...args: ArgType[]) {
   logger.error(stringifyArgs(...args));
 }
 
@@ -65,7 +102,7 @@ export function setLevel(level: string) {
   logger.level = process.env.LOG_LEVEL;
 }
 
-export function die(text?: string): never {
-  if (text) logger.error(text);
+export function die(...args: ArgType[]): never {
+  error(...args);
   process.exit(1);
 }
